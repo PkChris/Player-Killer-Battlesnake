3735ec77a42cad2ebe7ad5dd4b490748ba36b0ea
pkchris <pkchris@gmail.com>
2019-11-28 15:14:22 -0500

    debugging

--- index.js
+++ index.js
@@ -38,10 +38,33 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  var shuffle = function (array) {
+
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  
+  };
+
+  var directions = ['up', 'down', 'left', 'right'];
+  shuffle(directions);
 
   // Response data
   const data = {
-    move: 'right', // one of: ['up','down','left','right']
+    move: directions[0], // one of: ['up','down','left','right']
   }
 
   return response.json(data)
